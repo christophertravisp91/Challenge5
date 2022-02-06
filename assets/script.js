2f3944a288b4bb50e8fe5ec95f382996eccd46c1
TomFoolery <christophertravisp91@gmail.com>
2022-02-06 11:21:40 -0500

    finished code

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,53 @@
-setInterval(function(){ 
-    var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
-    document.getElementById ("currentDay").innerText = currentDay;
-}, 1000);
+$(document).ready(function () {
+
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+    $(".saveBtn").on("click", function () {
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
 
 
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
-// saving to localStorage
-localStorage.setItem('firstName', 'Michell');
+function timeTrack() {
 
-// getting a value from localStorage
-console.log(localStorage.getItem('firstName'));
+    var currentTime = moment().hour();
+
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        if (blockTime < currentTime) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === currentTime) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else{
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+
+timeTrack();
+
+})
 
 
-let names = ['Jane', "Eric", "Sarah"];
-//convert array to string because localStorage only takes a string
-localStorage.setItem('names', JSON.stringify(names));
-
-
-// convert string back into an array that we got from localStorage
-let namesVal = JSON.parse(localStorage.getItem('names'));
-console.log(namesVal);
